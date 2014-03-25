e8cb4e6f876e4e826470171450c084742e802b4a
DigohD <digohd1337@gmail.com>
2014-03-25 12:55:03 +0100

    Rensade - Ny Gameloop och Canvas

---
+++ src/com/example/se_android/GameWorld.java
@@ -0,0 +1,5 @@
+package com.example.se_android;
+
+public class GameWorld {
+
+}
--- src/com/example/se_android/MainView.java
+++ src/com/example/se_android/MainView.java
@@ -19,42 +19,23 @@ public class MainView extends View{
 	
 	static MediaPlayer mp; 
 	
-	ArrayList<Stripes> stripes = new ArrayList<Stripes>();
-	
-	Semaphore s = new Semaphore(1);
-	
-	boolean jump;
-	int jumpStacks;
-	
 	float finalWidth;
 	float finalHeight;
 	boolean ratioCalc, music;
 	
-	Player player;
-	
-	Ground g;
-	
-	int points = 0;
-	
 	Context c;
-	
-	float cX = 200, cY = 500;
 	
     public MainView(Context context){
         super(context);
-        paint.setColor(Color.RED);
-        
-        g = new Ground(0, 650, 2000, 200);
-        player = new Player(cX, cY, paint);
         
         c = context;
     }
 
     public void playSound(Context context){      
-    	mp = MediaPlayer.create(context, R.raw.n);
-    	if(!mp.isPlaying()){
-        	mp.start();
-    	}
+//    	mp = MediaPlayer.create(context, R.raw.n);
+//    	if(!mp.isPlaying()){
+//        	mp.start();
+//    	}
     }
     
     @Override
@@ -62,10 +43,10 @@ public class MainView extends View{
     	// x 1920
     	// y 885
     	
-    	if(!music){
-    		playSound(c);
-    		music = true;
-    	}
+//    	if(!music){
+//    		playSound(c);
+//    		music = true;
+//    	}
     	
     	if(!ratioCalc){
     		float newWidth = canvas.getWidth();
@@ -76,59 +57,12 @@ public class MainView extends View{
     	}
     	
     	canvas.scale(finalWidth, finalHeight);
-    	
-    	try {
-			s.acquire();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-        for(Stripes x : stripes){
-        	paint.setColor(Color.WHITE);
-    		canvas.drawLine(x.getX(), x.getY(), x.getX() + 100, x.getY(), paint);
-    	}
-        s.release();
-    	
-    	g.draw(canvas);
-    	
-    	paint.setColor(Color.RED);
-    	paint.setStyle(Paint.Style.FILL);
-    	canvas.drawRect(player.getRect(), paint);
-    	
-    	paint.setColor(Color.WHITE);
-    	paint.setStyle(Paint.Style.FILL);
-    	paint.setTextScaleX(3);
-    	paint.setTextSize(50);
-    	canvas.drawText("Score: " + points, 40, 80, paint);
-    	
-    	
-        
     }
 	
     @Override
 	    public boolean onTouchEvent(MotionEvent event) {
 	    float eventX = event.getX();
 	    float eventY = event.getY();
-	
-	    if(event.getAction() == event.ACTION_DOWN){
-	    	jump = true;
-		    switch(rnd.nextInt(5)){
-		    	case 0:
-		    		paint.setColor(Color.BLUE);
-		    		break;
-		    	case 1:
-		    		paint.setColor(Color.GREEN);
-		    		break;
-		    	case 2:
-		    		paint.setColor(Color.WHITE);
-		    		break;
-		    	case 3:
-		    		paint.setColor(Color.YELLOW);
-		    		break;
-		    	case 4:
-		    		paint.setColor(Color.RED);
-		    		break;
-		    }
-	    }
       
     	// Schedules a repaint.
     	invalidate();
@@ -136,50 +70,7 @@ public class MainView extends View{
     }
     
     public void tick(){
-    	if(jump)
-    		jump();
     	
-    	try {
-			s.acquire();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	if(rnd.nextInt(2) == 0)
-    		stripes.add(new Stripes());
-    	
-    	for(Stripes x : stripes)
-    		x.tick();
-    	s.release();
-    	
-    	for(int i = 0; i < g.getObstacles().size(); i++){
-    		player.collision(g.getObstacles().get(i));
-    		if(!player.isLive()){
-    			
-    		}
-    		
-    	}
-    	
-    	g.tick();
-    	
-    	
-    	player.tick();
-    	
-    	points++;
-    }
-    
-    private void jump(){
-    	jumpStacks++;
-    	if(jumpStacks < 15)
-    		cY -= 46;
-    	else if(jumpStacks >= 15 && jumpStacks < 29)
-    		cY += 46;
-    	else if(jumpStacks >= 29){
-    		jump = false;
-    		jumpStacks = 0;
-    	}
-    	player.setX(cX);
-    	player.setY(cY);
     }
     
 }
