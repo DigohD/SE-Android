fd2dcd0418365a6ecda88de36a4d6ead24254211
anduurs <anders__e92@hotmail.com>
2014-04-01 15:22:42 +0200

    Player bitmAp

--- src/com/example/se_android/MainActivity.java
+++ src/com/example/se_android/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends Activity {
 	        gameView = new GameView(this);
 	        setContentView(gameView);
 	        
-	        requestWindowFeature(Window.FEATURE_NO_TITLE);
 	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	        
