62e60a3dd7f75220bede58bd54cb2df5e24fe1d8
Jonas Enlund <enlund.jonas@gmail.com>
2014-04-07 15:29:19 +0200

    Added menu package

---
+++ src/com/spaceshooter/game/menu/MenyActivity.java
@@ -0,0 +1,10 @@
+package com.spaceshooter.game.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenyActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
