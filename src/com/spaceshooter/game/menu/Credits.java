1f13ce78e578923d3e8f78e71a33f694625e2eca
IsaAzc <isabelazcarate89@gmail.com>
2014-04-09 13:35:27 +0200

    Updated comment

--- src/com/spaceshooter/game/menu/Credits.java
+++ src/com/spaceshooter/game/menu/Credits.java
@@ -3,5 +3,5 @@ package com.spaceshooter.game.menu;
 import android.app.Activity;
 
 public class Credits extends Activity {
-	// TODO Enter code for displaying credits
+	// TODO Enter code for displaying credits. Names and homepage.
 }
