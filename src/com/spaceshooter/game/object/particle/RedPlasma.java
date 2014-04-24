dff7e0326a4a6029c1290d2f2cae9edfcd3d0ced
Digohd <Digohd1337@gmail.com>
2014-04-24 17:06:16 +0200

    Particles started

--- src/com/spaceshooter/game/object/particle/RedPlasma.java
+++ src/com/spaceshooter/game/object/particle/RedPlasma.java
@@ -1,29 +1,30 @@
-package com.spaceshooter.game.object.projectile;
+package com.spaceshooter.game.object.particle;
 
 import android.graphics.Rect;
 
+import com.spaceshooter.game.engine.GameObjectManager;
 import com.spaceshooter.game.engine.ProjectileManager;
 import com.spaceshooter.game.util.BitmapHandler;
 import com.spaceshooter.game.util.Vector2f;
 import com.spaceshooter.game.view.GameView;
 
-public class RedPlasma extends Projectile{
-
-	public RedPlasma(Vector2f position) {
+public class RedPlasma extends Particle{
+	
+	public RedPlasma(Vector2f position, Vector2f velocity) {
 		super(position, new Vector2f(75f, 0f));
+		this.bitmap = BitmapHandler.loadBitmap("particles/PlasmaRed");
+		this.velocity = velocity;
+
+		timeLived = 0;
+		lifetime = 60;
 		
-		this.bitmap = BitmapHandler.loadBitmap("projectiles/PlasmaRed");
-		this.width = bitmap.getWidth();
-		this.height = bitmap.getHeight();
-	
-		rect = new Rect((int)position.x, (int)position.y, (int)position.x + width, (int)position.y + height);
-	
-		ProjectileManager.addPlayerProjectile(this);
+		GameObjectManager.addGameObject(this);
 	}
 	
 	@Override
 	public void tick(float dt){
-		if(this.getX() > GameView.width)
+		timeLived++;
+		if(timeLived > lifetime)
 			live = false;
 		
 		position = position.add(velocity.mul(dt));
