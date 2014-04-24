fdff3f8119abc829daf1bac4cff97eaea9a54805
Digohd <Digohd1337@gmail.com>
2014-04-24 16:33:08 +0200

    First Projectile Done

--- src/com/spaceshooter/game/object/projectile/Projectile.java
+++ src/com/spaceshooter/game/object/projectile/Projectile.java
@@ -1,13 +1,18 @@
 package com.spaceshooter.game.object.projectile;
 
+import android.graphics.Rect;
+
 import com.spaceshooter.game.object.DynamicObject;
+import com.spaceshooter.game.util.BitmapHandler;
 import com.spaceshooter.game.util.Vector2f;
 
 public abstract class Projectile extends DynamicObject{
 
-	public Projectile(Vector2f position) {
+	public Projectile(Vector2f position, Vector2f velocity) {
 		super(position);
-		
+		this.velocity = velocity;
 	}
+	
+	
 
 }
---
+++ src/com/spaceshooter/game/object/projectile/RedPlasma.java
@@ -0,0 +1,34 @@
+package com.spaceshooter.game.object.projectile;
+
+import android.graphics.Rect;
+
+import com.spaceshooter.game.engine.ProjectileManager;
+import com.spaceshooter.game.util.BitmapHandler;
+import com.spaceshooter.game.util.Vector2f;
+import com.spaceshooter.game.view.GameView;
+
+public class RedPlasma extends Projectile{
+
+	public RedPlasma(Vector2f position) {
+		super(position, new Vector2f(15f, 0f));
+		
+		this.bitmap = BitmapHandler.loadBitmap("projectiles/PlasmaRed.png");
+		this.width = bitmap.getWidth();
+		this.height = bitmap.getHeight();
+	
+		rect = new Rect((int)position.x, (int)position.y, (int)position.x + width, (int)position.y + height);
+	
+		ProjectileManager.addPlayerProjectile(this);
+	}
+	
+	@Override
+	public void tick(float dt){
+		if(this.getX() > GameView.width)
+			live = false;
+		
+		position.add(velocity.mul(dt));
+		
+		rect.set((int)position.x, (int)position.y, (int)position.x + width, (int)position.y + height);
+	}
+	
+}
