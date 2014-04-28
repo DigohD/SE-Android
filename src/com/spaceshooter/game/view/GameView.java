a62dc8a8995c01eb2730c98c542513144138b03d
Digohd <Digohd1337@gmail.com>
2014-04-28 16:52:39 +0200

    Scaling debug

--- src/com/spaceshooter/game/view/GameView.java
+++ src/com/spaceshooter/game/view/GameView.java
@@ -51,12 +51,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	@Override
 	public void draw(Canvas canvas){
 		super.draw(canvas);
-		float scaleX = canvas.getWidth() / 1920;
-		float scaleY = canvas.getHeight() / 1080;
-
-		//System.out.println(scaleX + " - " + scaleY);
-		
-		canvas.scale(scaleX, scaleY);
 		
 		//clear the screen with black pixels
 		canvas.drawColor(Color.BLACK);
