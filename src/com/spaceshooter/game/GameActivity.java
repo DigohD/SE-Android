f8f439d6db6ef75d51bd1831f941e4bd45862bc8
anduurs <anders__e92@hotmail.com>
2014-05-12 14:03:12 +0200

    old database code removed

--- src/com/spaceshooter/game/GameActivity.java
+++ src/com/spaceshooter/game/GameActivity.java
@@ -1,10 +1,5 @@
 package com.spaceshooter.game;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
@@ -15,38 +10,15 @@ import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.example.se_android.R;
-import com.spaceshooter.game.database.HighScoreDataHelper;
-import com.spaceshooter.game.database.HighScoreEntry;
 import com.spaceshooter.game.engine.GameObjectManager;
-import com.spaceshooter.game.object.projectile.Projectile;
-import com.spaceshooter.game.object.projectile.enemy.MantisProj;
-import com.spaceshooter.game.object.projectile.enemy.PredatorProj;
-import com.spaceshooter.game.object.projectile.player.BluePlasma;
-import com.spaceshooter.game.object.projectile.player.GreenPlasma;
-import com.spaceshooter.game.object.projectile.player.RedPlasma;
 import com.spaceshooter.game.util.BitmapHandler;
-import com.spaceshooter.game.util.Randomizer;
 import com.spaceshooter.game.util.SoundPlayer;
-import com.spaceshooter.game.util.Vector2f;
 import com.spaceshooter.game.view.GameView;
 import com.spaceshooter.game.view.InventoryView;
 
 public class GameActivity extends Activity {
 
-	public static SimpleDateFormat myDateFormat = new SimpleDateFormat("MMM d");
-	private HighScoreDataHelper highScoreAccessor;
-
-	private Button submitButton;
-	private Button showScoresButton;
-
-	private EditText playerName;
-	private EditText gameId;
-	private EditText score;
 
 	private GameView gameView;
 	private InventoryView invView;
@@ -58,7 +30,6 @@ public class GameActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		// we'll use this to manipulate the list of high scores
 
-//		highScoreAccessor = new HighScoreDataHelper(getApplicationContext());
 		
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
@@ -86,31 +57,8 @@ public class GameActivity extends Activity {
         
         new SoundPlayer(this);
         
-    	// we'll use this to manipulate the list of high scores
-// 		highScoreAccessor = new HighScoreDataHelper(getApplicationContext());
-//
-// 		submitButton = (Button) findViewById(R.id.submitButton);
-// 		submitButton.setOnClickListener(new View.OnClickListener() {
-// 			@Override
-// 			public void onClick(View v) {
-// 				submitScore();
-// 			}
-// 		});
-//
-// 		showScoresButton = (Button) findViewById(R.id.showScoresButton);
-// 		showScoresButton.setOnClickListener(new View.OnClickListener() {
-// 			@Override
-// 			public void onClick(View v) {
-// 				Intent highScoreListActivity = new Intent(getBaseContext(), HighScoreList.class);
-// 				highScoreListActivity.putExtra(HighScoreList.EXTRA_GAME_ID, gameId.getText().toString());
-// 				startActivity(highScoreListActivity);
-// 			}
-// 		});
-//
-// 		gameId = (EditText) findViewById(R.id.gameId);
-// 		playerName = (EditText) findViewById(R.id.playerName);
-// 		score = (EditText) findViewById(R.id.score);
-//        
+
+       
 
 	}
 	
@@ -185,53 +133,9 @@ public class GameActivity extends Activity {
 //		 gameView.resume();
 //	 }
 
-	// throw the score at the database
-	private void submitScore() {
-		HighScoreEntry entry = null;
-		try {
-			entry = new HighScoreEntry(playerName.getText().toString(),
-					new Date(), Double.valueOf(score.getText().toString()),
-					Long.valueOf(gameId.getText().toString()));
-		} catch (NumberFormatException e) {
-			Toast.makeText(getApplicationContext(), R.string.badInput,
-					Toast.LENGTH_LONG).show();
-			return;
-		}
+	
 
-		double highest = Double.NEGATIVE_INFINITY;
-		List<HighScoreEntry> scores = highScoreAccessor.getByGameId(entry
-				.getGameId());
-		if (scores.size() > 0) {
-			for (HighScoreEntry hse : scores) { // simply look for the highest
-												// existing score
-				if (highest < hse.getScore()) {
-					highest = hse.getScore();
-				}
-			}
-		}
-
-		// give a little "congratulations" message if yours is the highest score
-		// so far
-		if (highest < entry.getScore()) { // you beat everyone else (including
-											// yourself) on this game!
-			Toast newHighScore = Toast.makeText(getApplicationContext(),
-					R.string.newHighScore, Toast.LENGTH_LONG);
-			newHighScore.show();
-		} else {
-			Toast.makeText(getApplicationContext(), R.string.saving,
-					Toast.LENGTH_LONG).show();
-		}
-
-		// well, we could probably push this into the HighScoreDataHelper
-		if (highScoreAccessor.alreadyExists(entry)) { // only store a single
-														// score per
-														// player/gameID
-														// combination
-			highScoreAccessor.update(entry);
-		} else {
-			highScoreAccessor.insert(entry);
-		}
-	}
+		
 	
 //UNIT TESTS
 	
