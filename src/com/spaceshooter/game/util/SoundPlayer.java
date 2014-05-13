c83cd9c734b5e888e0796556fb8fc86b4abbecac
DigohD <digohd1337@gmail.com>
2014-05-13 17:07:46 +0200

    Sounds for Yellow Plasma Gun

--- src/com/spaceshooter/game/util/SoundPlayer.java
+++ src/com/spaceshooter/game/util/SoundPlayer.java
@@ -19,9 +19,9 @@ public class SoundPlayer {
 	
 	public enum SoundID {
 		nullValue,
-		p,
-		fire_RedPlasma, fire_BluePlasma, fire_GreenPlasma,
-		hit_RedPlasma, hit_BluePlasma, hit_GreenPlasma,
+		pick,
+		fire_RedPlasma, fire_BluePlasma, fire_GreenPlasma, fire_YellowPlasma,
+		hit_RedPlasma, hit_BluePlasma, hit_GreenPlasma, hit_YellowPlasma,
 		exp_1, exp_2
 	}
 	
@@ -38,14 +38,21 @@ public class SoundPlayer {
 	    sounds = new HashMap<SoundID, Integer>();
 	    		
 	    sounds.put(SoundID.nullValue, soundPool.load(activity, com.example.se_android.R.raw.laser, 1));
+	    
+	    sounds.put(SoundID.pick, soundPool.load(activity, com.example.se_android.R.raw.pick, 1));
+	    
 	    sounds.put(SoundID.exp_1, soundPool.load(activity, com.example.se_android.R.raw.exp_1, 1));
 	    sounds.put(SoundID.exp_2, soundPool.load(activity, com.example.se_android.R.raw.exp_2, 1));
+	    
 	    sounds.put(SoundID.fire_RedPlasma, soundPool.load(activity, com.example.se_android.R.raw.fire_redplasma, 1));
 	    sounds.put(SoundID.fire_BluePlasma, soundPool.load(activity, com.example.se_android.R.raw.fire_blueplasma, 1));
 	    sounds.put(SoundID.fire_GreenPlasma, soundPool.load(activity, com.example.se_android.R.raw.fire_greenplasma, 1));
+	    sounds.put(SoundID.fire_YellowPlasma, soundPool.load(activity, com.example.se_android.R.raw.fire_yellowplasma, 1));
+	    
 	    sounds.put(SoundID.hit_RedPlasma, soundPool.load(activity, com.example.se_android.R.raw.hit_redplasma, 1));
 	    sounds.put(SoundID.hit_BluePlasma, soundPool.load(activity, com.example.se_android.R.raw.hit_blueplasma, 1));
 	    sounds.put(SoundID.hit_GreenPlasma, soundPool.load(activity, com.example.se_android.R.raw.hit_greenplasma, 1));
+	    sounds.put(SoundID.hit_YellowPlasma, soundPool.load(activity, com.example.se_android.R.raw.hit_yellowplasma, 1));
 	}
 	
 	public static void playSound(SoundID ID){
