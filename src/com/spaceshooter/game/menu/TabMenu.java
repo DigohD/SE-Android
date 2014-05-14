5a81b76ec77919c00a74b0482c29594a92081320
Jonas Enlund <enlund.jonas@gmail.com>
2014-05-14 17:11:27 +0200

    bg

--- src/com/spaceshooter/game/menu/TabMenu.java
+++ src/com/spaceshooter/game/menu/TabMenu.java
@@ -5,9 +5,12 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.pm.ActivityInfo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.ToggleButton;
@@ -24,12 +27,19 @@ public class TabMenu extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.tabs);
 		AdView adView = (AdView) this.findViewById(R.id.adView);
 		AdRequest adRequest = new AdRequest.Builder()
 				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR) // Emulator
-				.addTestDevice("CC224A050390619FD22B9448CC95A60D") // Jonas Nexus 4
-				.addTestDevice("e83ab40d") // Simons Note (var fel värde, leta upp rätt under körning i LogCat
+				.addTestDevice("CC224A050390619FD22B9448CC95A60D") // Jonas
+																	// Nexus 4
+				.addTestDevice("e83ab40d") // Simons Note (var fel värde, leta
+											// upp rätt under körning i LogCat
 				.addTestDevice("0009478f6e129f") // Anders Galaxy S2
 				.build();
 		adView.loadAd(adRequest);
