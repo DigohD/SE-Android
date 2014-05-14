b3731c46d8a331c864ab07709bd2f233a2e4ba10
Jonas Enlund <enlund.jonas@gmail.com>
2014-05-14 16:17:45 +0200

    music setting now working

--- src/com/spaceshooter/game/GameActivity.java
+++ src/com/spaceshooter/game/GameActivity.java
@@ -25,207 +25,212 @@ import com.spaceshooter.game.view.InventoryView;
 
 public class GameActivity extends Activity {
 
-
 	private GameView gameView;
 	private InventoryView invView;
-	public boolean musicState; // is false if music shouldn't be played and true if it should
+	public boolean musicState; // is false if music shouldn't be played and true
+								// if it should
 	public boolean isInvView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// we'll use this to manipulate the list of high scores
-		
+
 		// used to get if the music toggle is clicked or not
 		Intent intent = getIntent();
 		musicState = intent.getBooleanExtra("EXTRA_musicState", true);
-		
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
-        getWindow().setFlags(
-        	    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-        	    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-        
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        
-        getWindow().setFormat(PixelFormat.RGBA_8888);
-        
-        new BitmapHandler(this);
-        
-//        gameView = new GameView(this);
-//        setContentView(gameView);
-        
-        GameObjectManager go = new GameObjectManager();
-        
-        isInvView = true;
-        invView = new InventoryView(this);
-        setContentView(invView);
-        
-        new SoundPlayer(this);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		getWindow().setFlags(
+				WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+				WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+		getWindow().setFormat(PixelFormat.RGBA_8888);
+
+		new BitmapHandler(this);
+
+		// gameView = new GameView(this);
+		// setContentView(gameView);
+
+		GameObjectManager go = new GameObjectManager();
+
+		isInvView = true;
+		invView = new InventoryView(this);
+		setContentView(invView);
+
+		new SoundPlayer(this);
 	}
-	
+
 	public void database() {
 		Intent intent = new Intent(this, DatabaseActivity.class);
 		finish();
 		startActivity(intent);
 	}
-	
-	private void exitDialog(){
+
+	private void exitDialog() {
 		gameView.pause();
 		Builder builder = new AlertDialog.Builder(this);
 		builder.setCancelable(false);
 		builder.setTitle("Quit Game?");
 		builder.setMessage("Return to main menu?");
 		builder.setNegativeButton("Cancel", new OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-                gameView.resume();
-            }});
+			public void onClick(DialogInterface arg0, int arg1) {
+				gameView.resume();
+			}
+		});
 		builder.setPositiveButton("Ok", new OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-            	gameView.stop();
-                GameActivity.super.onBackPressed();
-            }});
-		
+			public void onClick(DialogInterface arg0, int arg1) {
+				gameView.stop();
+				GameActivity.super.onBackPressed();
+			}
+		});
+
 		builder.create().show();
 	}
-	
-	private void exitDialogInv(){
+
+	private void exitDialogInv() {
 		invView.pause();
 		Builder builder = new AlertDialog.Builder(this);
 		builder.setCancelable(false);
 		builder.setTitle("Quit Game?");
 		builder.setMessage("Return to main menu?");
 		builder.setNegativeButton("Cancel", new OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-            	invView.resume();
-            }});
+			public void onClick(DialogInterface arg0, int arg1) {
+				invView.resume();
+			}
+		});
 		builder.setPositiveButton("Ok", new OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-            	invView.stop();
-                GameActivity.super.onBackPressed();
-            }});
-		
+			public void onClick(DialogInterface arg0, int arg1) {
+				invView.stop();
+				GameActivity.super.onBackPressed();
+			}
+		});
+
 		builder.create().show();
 	}
-	
-	public void goToGame(){
+
+	public void goToGame() {
 		isInvView = false;
 		gameView = new GameView(this);
 		setContentView(gameView);
 	}
-	
-	public void onBackPressed2(){
-		 GameActivity.super.onBackPressed();
+
+	public void onBackPressed2() {
+		GameActivity.super.onBackPressed();
 	}
-	
+
 	@Override
 	public void onBackPressed() {
-		
-		if(isInvView)
+
+		if (isInvView)
 			exitDialogInv();
-		else exitDialog();
+		else
+			exitDialog();
 	}
-	
-//	public void onStop(){
-//		super.onStop();
-//		gameView.pause();
-//	}
-//
-//	 public void onPause(){
-//		 super.onPause();
-//		 gameView.pause();
-//	 }
-//	
-//	 public void onResume(){
-//		 super.onResume();
-//		 gameView.resume();
-//	 }
 
-	
+	// public void onStop(){
+	// super.onStop();
+	// gameView.pause();
+	// }
+	//
+	// public void onPause(){
+	// super.onPause();
+	// gameView.pause();
+	// }
+	//
+	// public void onResume(){
+	// super.onResume();
+	// gameView.resume();
+	// }
 
-		
-	
-//UNIT TESTS
-	
-//	public void testCalculatePlayerScore(){
-//		Enemy e = new Predator(new Vector2f(0,0), 40, 40);
-//		// 10 * 1.2^1 * 5
-//		int test1 = e.calculatePlayerScore(10, 1, 5);
-//		int test2 = e.calculatePlayerScore(20, 2, 8);
-//		int test3 = e.calculatePlayerScore(15, 3, 2);
-//		int test4 = e.calculatePlayerScore(30, 4, 4);
-//		int test5 = e.calculatePlayerScore(50, 5, 12);
-//		
-//		assertEquals(60, test1);
-//		assertEquals(230, test2);
-//		assertEquals(51, test3);
-//		assertEquals(248, test4);
-//		assertEquals(1492, test5);
-//	}
-	
-//	public void testVector2fNormalize(){
-//		for(int i = 0; i < 10000; i++){
-//			float rX = Randomizer.getFloat(1, 800);
-//			float rY = Randomizer.getFloat(1, 480);
-//			Vector2f v = new Vector2f(rX, rY);
-//			assertEquals(1.0f, v.normalize().length());
-//		}
-//	}
-//	
-//	public void testVector2fDot(){
-//		Vector2f v1 = new Vector2f(5, 6);
-//		Vector2f v2 = new Vector2f(3, 7);
-//		
-//		Vector2f v3 = new Vector2f(15, 5);
-//		Vector2f v4 = new Vector2f(12, 8);
-//		
-//		Vector2f v5 = new Vector2f(11, 27);
-//		Vector2f v6 = new Vector2f(38, 70);
-//		
-//		Vector2f v7 = new Vector2f(130, 78);
-//		Vector2f v8 = new Vector2f(56, 18);
-//		
-//		assertEquals(57.0f, v1.dot(v2));
-//		assertEquals(220.0f, v3.dot(v4));
-//		assertEquals(2308.0f, v5.dot(v6));
-//		assertEquals(8684.0f, v7.dot(v8));
-//	}
-	
-//	public void testProjectileOutOfBound(){
-//		List<Projectile> projectiles = new ArrayList<Projectile>();
-//		
-//		Projectile p1 = new RedPlasma(new Vector2f(GameView.WIDTH,0), 20, 5);
-//		Projectile p2 = new BluePlasma(new Vector2f(GameView.WIDTH/2, -12), 12, 12);
-//		Projectile p3 = new GreenPlasma(new Vector2f(GameView.WIDTH/2, GameView.HEIGHT+12), 30, 12);
-//		Projectile p4 = new MantisProj(new Vector2f(-20,GameView.HEIGHT/2), 20, 2);
-//		Projectile p5 = new PredatorProj(new Vector2f(-20,GameView.HEIGHT/2), 20, 5);
-//		
-//		projectiles.add(p1);
-//		projectiles.add(p2);
-//		projectiles.add(p3);
-//		projectiles.add(p4);
-//		projectiles.add(p5);
-//		
-//		boolean[] projsOutofBound = new boolean[5];
-//		
-//		for(int i = 0; i < projsOutofBound.length; i++)
-//			projsOutofBound[i] = false;
-//		
-//		for(int i = 0; i < projectiles.size(); i++){
-//			Projectile p = projectiles.get(i);
-//			if(p.isOutOfBound())
-//				projsOutofBound[i] = true;
-//		}
-//		
-//		int count = 0;
-//		for(int i = 0; i < projsOutofBound.length; i++)
-//			if(projsOutofBound[i])
-//				count++;
-//			
-//		assertEquals(projectiles.size(), count);
-//	}
+	// UNIT TESTS
+
+	// public void testCalculatePlayerScore(){
+	// Enemy e = new Predator(new Vector2f(0,0), 40, 40);
+	// // 10 * 1.2^1 * 5
+	// int test1 = e.calculatePlayerScore(10, 1, 5);
+	// int test2 = e.calculatePlayerScore(20, 2, 8);
+	// int test3 = e.calculatePlayerScore(15, 3, 2);
+	// int test4 = e.calculatePlayerScore(30, 4, 4);
+	// int test5 = e.calculatePlayerScore(50, 5, 12);
+	//
+	// assertEquals(60, test1);
+	// assertEquals(230, test2);
+	// assertEquals(51, test3);
+	// assertEquals(248, test4);
+	// assertEquals(1492, test5);
+	// }
+
+	// public void testVector2fNormalize(){
+	// for(int i = 0; i < 10000; i++){
+	// float rX = Randomizer.getFloat(1, 800);
+	// float rY = Randomizer.getFloat(1, 480);
+	// Vector2f v = new Vector2f(rX, rY);
+	// assertEquals(1.0f, v.normalize().length());
+	// }
+	// }
+	//
+	// public void testVector2fDot(){
+	// Vector2f v1 = new Vector2f(5, 6);
+	// Vector2f v2 = new Vector2f(3, 7);
+	//
+	// Vector2f v3 = new Vector2f(15, 5);
+	// Vector2f v4 = new Vector2f(12, 8);
+	//
+	// Vector2f v5 = new Vector2f(11, 27);
+	// Vector2f v6 = new Vector2f(38, 70);
+	//
+	// Vector2f v7 = new Vector2f(130, 78);
+	// Vector2f v8 = new Vector2f(56, 18);
+	//
+	// assertEquals(57.0f, v1.dot(v2));
+	// assertEquals(220.0f, v3.dot(v4));
+	// assertEquals(2308.0f, v5.dot(v6));
+	// assertEquals(8684.0f, v7.dot(v8));
+	// }
+
+	// public void testProjectileOutOfBound(){
+	// List<Projectile> projectiles = new ArrayList<Projectile>();
+	//
+	// Projectile p1 = new RedPlasma(new Vector2f(GameView.WIDTH,0), 20, 5);
+	// Projectile p2 = new BluePlasma(new Vector2f(GameView.WIDTH/2, -12), 12,
+	// 12);
+	// Projectile p3 = new GreenPlasma(new Vector2f(GameView.WIDTH/2,
+	// GameView.HEIGHT+12), 30, 12);
+	// Projectile p4 = new MantisProj(new Vector2f(-20,GameView.HEIGHT/2), 20,
+	// 2);
+	// Projectile p5 = new PredatorProj(new Vector2f(-20,GameView.HEIGHT/2), 20,
+	// 5);
+	//
+	// projectiles.add(p1);
+	// projectiles.add(p2);
+	// projectiles.add(p3);
+	// projectiles.add(p4);
+	// projectiles.add(p5);
+	//
+	// boolean[] projsOutofBound = new boolean[5];
+	//
+	// for(int i = 0; i < projsOutofBound.length; i++)
+	// projsOutofBound[i] = false;
+	//
+	// for(int i = 0; i < projectiles.size(); i++){
+	// Projectile p = projectiles.get(i);
+	// if(p.isOutOfBound())
+	// projsOutofBound[i] = true;
+	// }
+	//
+	// int count = 0;
+	// for(int i = 0; i < projsOutofBound.length; i++)
+	// if(projsOutofBound[i])
+	// count++;
+	//
+	// assertEquals(projectiles.size(), count);
+	// }
 
 }
--- src/com/spaceshooter/game/view/GameView.java
+++ src/com/spaceshooter/game/view/GameView.java
@@ -28,12 +28,12 @@ import com.spaceshooter.game.util.MusicPlayer;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
-	public static final int WIDTH = 800, HEIGHT = 480; 
-	
+	public static final int WIDTH = 800, HEIGHT = 480;
+
 	private int timer = 0, timer2 = 0;
 	private int levelTime = 30;
 	private static int levelID = 2;
-	
+
 	private float scaleX, scaleY;
 	private float knobX;
 	private float knobY;
@@ -41,7 +41,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	private boolean drawJoystick = true;
 	private boolean okToRestartMP = true;
 	private boolean newLevel = false, firstLevel = true;
-	
+	public boolean gwMusicState;
+
 	private Context context;
 	private SurfaceHolder holder;
 	private GameThread game;
@@ -50,33 +51,33 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	private MusicPlayer mp;
 	private Paint p = new Paint();
 	private int musicStartTimer;
-	
-	
+
 	public GameView(Context context) {
 		super(context);
 		this.context = context;
-	
+		GameActivity ga = (GameActivity) context;
+		gwMusicState = ga.musicState;
 		mp = null;
 		musicStartTimer = 0;
-		
+
 		level = new Level(levelTime);
-		game = new GameThread(getHolder(),this);
-		
+		game = new GameThread(getHolder(), this);
+
 		joystick = BitmapHandler.loadBitmap("ui/joystick");
 		knob = BitmapHandler.loadBitmap("ui/joystickKnob");
-		
-		knobX = 40 + (joystick.getWidth()/2) - (knob.getWidth()/2);
-		knobY = 320 + (joystick.getHeight()/2) - (knob.getHeight()/2);
-		
+
+		knobX = 40 + (joystick.getWidth() / 2) - (knob.getWidth() / 2);
+		knobY = 320 + (joystick.getHeight() / 2) - (knob.getHeight() / 2);
+
 		WindowManager wm = (WindowManager) context
 				.getSystemService(Context.WINDOW_SERVICE);
-		
+
 		Display display = wm.getDefaultDisplay();
 		Point size = new Point();
 		display.getSize(size);
-		
-		scaleX = ((float)size.x / (float)WIDTH);
-		scaleY = ((float)size.y / (float)HEIGHT);
+
+		scaleX = ((float) size.x / (float) WIDTH);
+		scaleY = ((float) size.y / (float) HEIGHT);
 
 		setLayerType(View.LAYER_TYPE_HARDWARE, null);
 		holder = getHolder();
@@ -85,95 +86,112 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
 		holder.addCallback(this);
 	}
-	
+
 	/**
 	 * Creates a dialogbox on the screen with a title, message and two buttons
 	 * one button for yes and one for no
-	 * @param title the title of the box, eg "Level completed!" or "You died"
-	 * @param msg the message in the box, eg "Restart the level?"
-	 * @param positiveBtn the text in the positive button
-	 * @param negativeBtn the text in the negative button
+	 * 
+	 * @param title
+	 *            the title of the box, eg "Level completed!" or "You died"
+	 * @param msg
+	 *            the message in the box, eg "Restart the level?"
+	 * @param positiveBtn
+	 *            the text in the positive button
+	 * @param negativeBtn
+	 *            the text in the negative button
 	 */
-	private void dialogBox(final String title, final String msg, final String positiveBtn,
-		final String neutralBtn, final String negativeBtn){
-		
+	private void dialogBox(final String title, final String msg,
+			final String positiveBtn, final String neutralBtn,
+			final String negativeBtn) {
+
 		final GameActivity ga = (GameActivity) context;
 		ga.runOnUiThread(new Runnable() {
-            public void run() {
-            	game.pause();
-            	mp.stop();
+			public void run() {
+				game.pause();
+				if (gwMusicState) {
+					mp.stop();
+				}
 
-            	Builder builder = new AlertDialog.Builder(context);
-        		builder.setCancelable(false);
-        		builder.setTitle(title);
-        		builder.setMessage(msg);
-        		builder.setNegativeButton(negativeBtn, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface arg0, int arg1) {
-                    	GameActivity ga2 = (GameActivity) context;
-                    	stop();
-                        ga2.onBackPressed2();
-                    }});
-        		builder.setNeutralButton(neutralBtn, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface arg0, int arg1) {
-                    	ga.database();
-                    	
-                    }});
-        		builder.setPositiveButton(positiveBtn, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface arg0, int arg1) {
-                    	GameObjectManager.clear();
-                    	level = new Level(levelTime);
-                    	GameObjectManager.getPlayer().init();
-                    	game.resume();
-                    	if(mp == null)
-                    		mp = new MusicPlayer(context);
-                    	okToRestartMP = true;
-                    	drawJoystick = true;
-                    	level.setFinished(false);
-    					timer = 0;
-    					timer2 = 0;
-    					firstLevel = true;
-    					levelID = 2;
-                    }});
-        		builder.create().show();
-        		
-            }});
-    	
+				Builder builder = new AlertDialog.Builder(context);
+				builder.setCancelable(false);
+				builder.setTitle(title);
+				builder.setMessage(msg);
+				builder.setNegativeButton(negativeBtn,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface arg0, int arg1) {
+								GameActivity ga2 = (GameActivity) context;
+								stop();
+								ga2.onBackPressed2();
+							}
+						});
+				builder.setNeutralButton(neutralBtn,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface arg0, int arg1) {
+								ga.database();
+
+							}
+						});
+				builder.setPositiveButton(positiveBtn,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface arg0, int arg1) {
+								GameObjectManager.clear();
+								level = new Level(levelTime);
+								GameObjectManager.getPlayer().init();
+								game.resume();
+								if (gwMusicState) {
+									if (mp == null)
+										mp = new MusicPlayer(context);
+								}
+								okToRestartMP = true;
+								drawJoystick = true;
+								level.setFinished(false);
+								timer = 0;
+								timer2 = 0;
+								firstLevel = true;
+								levelID = 2;
+							}
+						});
+				builder.create().show();
+
+			}
+		});
+
 	}
-	
-	
-	public void tick(float dt){
-		if(mp == null){
-			musicStartTimer++;
-			if(musicStartTimer > 10)
-				mp = new MusicPlayer(context);
+
+	public void tick(float dt) {
+		if (gwMusicState) {
+			if (mp == null) {
+				musicStartTimer++;
+				if (musicStartTimer > 10)
+					mp = new MusicPlayer(context);
+			}
 		}
-		
-		if(firstLevel)
+
+		if (firstLevel)
 			timer2++;
-		
-		if(timer2 >= 2*60)
+
+		if (timer2 >= 2 * 60)
 			firstLevel = false;
-		
+
 		level.tick(dt);
-		
-		if(level.isFinished() && GameObjectManager.getPlayer().isLive()){
+
+		if (level.isFinished() && GameObjectManager.getPlayer().isLive()) {
 			timer++;
-			
-			if(levelID >= Level.getNumOfLevels()+1)
+
+			if (levelID >= Level.getNumOfLevels() + 1)
 				newLevel = false;
-			else newLevel = true;
-			
-			if(timer >= 3*60){
+			else
+				newLevel = true;
+
+			if (timer >= 3 * 60) {
 				newLevel = false;
-				
-				if(levelID >= Level.getNumOfLevels()+1){
+
+				if (levelID >= Level.getNumOfLevels() + 1) {
 					okToRestartMP = false;
-					dialogBox("Game completed!", 
-					  "Highscore: " + GameObjectManager.getPlayer().getScore(), 
-					  "Restart",
-					  "LeaderBoard",
-					  "Main Menu" );
-				}else{
+					dialogBox("Game completed!", "Highscore: "
+							+ GameObjectManager.getPlayer().getScore(),
+							"Restart", "LeaderBoard", "Main Menu");
+				} else {
 					level.selectLevel(levelID);
 					levelID++;
 					level.setFinished(false);
@@ -181,122 +199,129 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 				}
 			}
 		}
-			
-		if(!GameObjectManager.getPlayer().isLive()){
+
+		if (!GameObjectManager.getPlayer().isLive()) {
 			okToRestartMP = false;
 			timer++;
-			if(timer >= 2*60){
-				dialogBox("You died! You made it to level " + (levelID-1), 
-						  "Highscore: " + GameObjectManager.getPlayer().getScore(),
-						  "Restart",
-						  "LeaderBoard",
-						  "Main Menu");
+			if (timer >= 2 * 60) {
+				dialogBox("You died! You made it to level " + (levelID - 1),
+						"Highscore: "
+								+ GameObjectManager.getPlayer().getScore(),
+						"Restart", "LeaderBoard", "Main Menu");
 				timer = 0;
-			}	
+			}
 		}
-
-		if(mp != null && mp.isDone() && okToRestartMP)
-			mp = new MusicPlayer(context);
+		if (gwMusicState) {
+			if (mp != null && mp.isDone() && okToRestartMP)
+				mp = new MusicPlayer(context);
+		}
 	}
-	
-	public void draw(Canvas canvas, float interpolation){
-		//clear the screen with black pixels
+
+	public void draw(Canvas canvas, float interpolation) {
+		// clear the screen with black pixels
 		canvas.drawColor(Color.BLACK);
-		//draw the level
+		// draw the level
 		level.draw(canvas, interpolation);
 
-		if(drawJoystick){
+		if (drawJoystick) {
 			canvas.drawBitmap(joystick, 40, 320, null);
 			canvas.drawBitmap(knob, knobX, knobY, null);
 		}
-		
-		if(newLevel){
+
+		if (newLevel) {
 			p.setColor(Color.GREEN);
-			canvas.drawText("Level " + (levelID), WIDTH/2, HEIGHT/2, p);
+			canvas.drawText("Level " + (levelID), WIDTH / 2, HEIGHT / 2, p);
 		}
-		
-		if(firstLevel){
+
+		if (firstLevel) {
 			p.setColor(Color.GREEN);
-			canvas.drawText("Level " + 1, WIDTH/2, HEIGHT/2, p);
+			canvas.drawText("Level " + 1, WIDTH / 2, HEIGHT / 2, p);
 		}
-			
+
 	}
 
 	public boolean onTouchEvent(MotionEvent event) {
-	    float eventX = event.getX();
-	    float eventY = event.getY();
-	    
-	    float nY = scaleY * 1.0f;
-	    
-	    eventX = eventX / scaleX;
-	    eventY = eventY / nY;
-		
-//	    System.out.println("Moved!!!! Game!!!");
-	    
-	    //Joystick center point: X = 100, Y = 380
-	    if(eventX < 210 && eventX > 0 && eventY < 480 && eventY > 300){
-	    	if(event.getAction() == MotionEvent.ACTION_MOVE){
-	    		
-	    		drawJoystick = true;
-	    		
-		    	if(eventX <= 180 && eventX >= 20 && eventY <= 470 && eventY >= 308){  
-		    		
-		    		knobX = eventX - knob.getWidth()/2;
-		    		knobY = eventY - knob.getHeight()/2;
-		    		
-		    		float dX = eventX - 100;
-			    	float dY = eventY - 380;
+		float eventX = event.getX();
+		float eventY = event.getY();
 
-			    	dX = dX / 8;
-			    	dY = dY / 8;
+		float nY = scaleY * 1.0f;
 
-			    	GameObjectManager.getPlayer().incTargetPos(dX, dY);
-			    	
-		    	}else {
-		    		GameObjectManager.getPlayer().setUpdate(false);
-		    		knobX = 40 + (joystick.getWidth()/2) - (knob.getWidth()/2);
-		    		knobY = 320 + (joystick.getHeight()/2) - (knob.getHeight()/2);
-		    	}
+		eventX = eventX / scaleX;
+		eventY = eventY / nY;
 
-	    	}
-	    	
-	    	if(event.getAction() == MotionEvent.ACTION_DOWN){
-	    		drawJoystick = true;
-	    	
-	    		if(eventX <= 180 && eventX >= 20 && eventY <= 470 && eventY >= 308){  
-		    		
-		    		knobX = eventX - knob.getWidth()/2;
-		    		knobY = eventY - knob.getHeight()/2;
-		    		
-		    		float dX = eventX - 100;
-			    	float dY = eventY - 380;
+		// System.out.println("Moved!!!! Game!!!");
 
-			    	dX = dX / 8;
-			    	dY = dY / 8;
+		// Joystick center point: X = 100, Y = 380
+		if (eventX < 210 && eventX > 0 && eventY < 480 && eventY > 300) {
+			if (event.getAction() == MotionEvent.ACTION_MOVE) {
 
-			    	GameObjectManager.getPlayer().incTargetPos(dX, dY);
-			    	
-		    	}else {
-		    		GameObjectManager.getPlayer().setUpdate(false);
-		    		knobX = 40 + (joystick.getWidth()/2) - (knob.getWidth()/2);
-		    		knobY = 320 + (joystick.getHeight()/2) - (knob.getHeight()/2);
-		    	}
-		    	
-		    	if(eventX > 700)
-		    		game.goInventory();
-		    }
-	    	
-	    	if(event.getAction() == MotionEvent.ACTION_UP){
-	    		drawJoystick = false;
-	    		knobX = 40 + (joystick.getWidth()/2) - (knob.getWidth()/2);
-	    		knobY = 320 + (joystick.getHeight()/2) - (knob.getHeight()/2);
-	    		GameObjectManager.getPlayer().setUpdate(false);
-	    	}
-	    }
-	    
-    	// Schedules a repaint.
-    	invalidate();
-    	return true;
+				drawJoystick = true;
+
+				if (eventX <= 180 && eventX >= 20 && eventY <= 470
+						&& eventY >= 308) {
+
+					knobX = eventX - knob.getWidth() / 2;
+					knobY = eventY - knob.getHeight() / 2;
+
+					float dX = eventX - 100;
+					float dY = eventY - 380;
+
+					dX = dX / 8;
+					dY = dY / 8;
+
+					GameObjectManager.getPlayer().incTargetPos(dX, dY);
+
+				} else {
+					GameObjectManager.getPlayer().setUpdate(false);
+					knobX = 40 + (joystick.getWidth() / 2)
+							- (knob.getWidth() / 2);
+					knobY = 320 + (joystick.getHeight() / 2)
+							- (knob.getHeight() / 2);
+				}
+
+			}
+
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				drawJoystick = true;
+
+				if (eventX <= 180 && eventX >= 20 && eventY <= 470
+						&& eventY >= 308) {
+
+					knobX = eventX - knob.getWidth() / 2;
+					knobY = eventY - knob.getHeight() / 2;
+
+					float dX = eventX - 100;
+					float dY = eventY - 380;
+
+					dX = dX / 8;
+					dY = dY / 8;
+
+					GameObjectManager.getPlayer().incTargetPos(dX, dY);
+
+				} else {
+					GameObjectManager.getPlayer().setUpdate(false);
+					knobX = 40 + (joystick.getWidth() / 2)
+							- (knob.getWidth() / 2);
+					knobY = 320 + (joystick.getHeight() / 2)
+							- (knob.getHeight() / 2);
+				}
+
+				if (eventX > 700)
+					game.goInventory();
+			}
+
+			if (event.getAction() == MotionEvent.ACTION_UP) {
+				drawJoystick = false;
+				knobX = 40 + (joystick.getWidth() / 2) - (knob.getWidth() / 2);
+				knobY = 320 + (joystick.getHeight() / 2)
+						- (knob.getHeight() / 2);
+				GameObjectManager.getPlayer().setUpdate(false);
+			}
+		}
+
+		// Schedules a repaint.
+		invalidate();
+		return true;
 	}
 
 	@Override
@@ -314,32 +339,38 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		stop();
 	}
-	
-	public void start(){
+
+	public void start() {
 		game.start();
 		GameObjectManager.getPlayer().init();
 	}
-	
+
 	public void stop() {
 		GameObjectManager.clear();
 		GameObjectManager.getPlayer().setScore(0);
-		mp.stop();
+		if (gwMusicState) {
+			mp.stop();
+		}
 		game.stop();
 	}
-	
+
 	public void pause() {
 		okToRestartMP = false;
-		mp.pause();
+		if (gwMusicState) {
+			mp.pause();
+		}
 		game.pause();
 	}
 
 	public void resume() {
 		okToRestartMP = true;
-		mp.resume();
+		if (gwMusicState) {
+			mp.resume();
+		}
 		game.resume();
 	}
-	
-	public static int getLevelID(){
+
+	public static int getLevelID() {
 		return levelID - 1;
 	}
 
