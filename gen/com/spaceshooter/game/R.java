69a4f862fe8ac78d88798f8ed806517cefceda77
Anders Eriksson <anders__e92@hotmail.com>
2014-05-16 20:44:41 +0200

    highscore view

--- gen/com/spaceshooter/game/R.java
+++ gen/com/spaceshooter/game/R.java
@@ -223,7 +223,8 @@ containing a value of this type.
         public static final int tab_bg_image=0x7f02001d;
     }
     public static final class id {
-        public static final int action_settings=0x7f06001c;
+        public static final int Button01=0x7f060016;
+        public static final int action_settings=0x7f06001d;
         public static final int adView=0x7f060010;
         public static final int btnAdd=0x7f060008;
         public static final int btnClear=0x7f060009;
@@ -234,16 +235,16 @@ containing a value of this type.
         public static final int clearHighScore=0x7f060014;
         public static final int container=0x7f06000f;
         public static final int hybrid=0x7f060004;
-        public static final int menu_delete=0x7f06001d;
+        public static final int menu_delete=0x7f06001e;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
         public static final int satellite=0x7f060002;
-        public static final int tabCredits=0x7f06001a;
-        public static final int tabMenu=0x7f060017;
-        public static final int tabScores=0x7f060018;
-        public static final int tabSettings=0x7f060019;
-        public static final int tab_bg=0x7f06001b;
-        public static final int tabhost=0x7f060016;
+        public static final int tabCredits=0x7f06001b;
+        public static final int tabMenu=0x7f060018;
+        public static final int tabScores=0x7f060019;
+        public static final int tabSettings=0x7f06001a;
+        public static final int tab_bg=0x7f06001c;
+        public static final int tabhost=0x7f060017;
         public static final int terrain=0x7f060003;
         public static final int textView1=0x7f060006;
         public static final int textView2=0x7f060005;
--- src/com/spaceshooter/game/view/GlobalHighScoreView.java
+++ src/com/spaceshooter/game/view/GlobalHighScoreView.java
@@ -4,35 +4,48 @@ import java.util.concurrent.ExecutionException;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
+import com.spaceshooter.tcp.HighScoreParser;
 import com.spaceshooter.tcp.TCPClient;
 
 public class GlobalHighScoreView extends View{
 	
 	public static final int WIDTH = 800, HEIGHT = 480;
 	
+	private String response;
+	private HighScoreParser parser;
+	private String[] entries;
+	Paint p = new Paint();
+	
 	public GlobalHighScoreView(Context context) {
 		super(context);
+		
+		parser = new HighScoreParser();
 		
 		TCPClient tcp = new TCPClient();
 		String[] querys = {"select"};
 		
 		try {
-			String response = tcp.execute(querys).get();
+			response = tcp.execute(querys).get();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (ExecutionException e) {
 			e.printStackTrace();
 		}
 		
-		tcp.execute(querys);
-	
+		entries = new String[parser.parseQuery(response).length];
 	}
 	
 	@Override
 	public void onDraw(Canvas c){
-		
+		c.drawColor(Color.BLACK);
+		p.setColor(Color.RED);
+		for(int i = 0; i < entries.length; i++){
+			c.drawText(entries[i], 200, i+5, p);
+		}
 	}
 	
 	
